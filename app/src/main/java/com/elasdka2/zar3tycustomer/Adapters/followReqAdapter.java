8b2bee41a074ba2f8de6d7f6846d86275b44b3c0
ahmed radwan <aradwan054@gmail.com>
2019-08-01 14:29:11 +0200

    t

--- app/src/main/java/com/elasdka2/zar3tycustomer/Adapters/followReqAdapter.java
+++ app/src/main/java/com/elasdka2/zar3tycustomer/Adapters/followReqAdapter.java
@@ -1,16 +1,25 @@
 package com.elasdka2.zar3tycustomer.Adapters;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
 import com.elasdka2.zar3tycustomer.Model.Follow;
 import com.elasdka2.zar3tycustomer.Model.Items;
+import com.elasdka2.zar3tycustomer.R;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
@@ -19,30 +28,45 @@ public class followReqAdapter extends RecyclerView.Adapter<followReqAdapter.View
     private Context context;
     private ArrayList<Follow> followsList;
     Follow follow;
+    String mImg;
     private DatabaseReference Ref;
 
+    public followReqAdapter(Context context, ArrayList<Follow> followsList) {
+        this.context = context;
+        this.followsList = followsList;
 
-
+    }
 
     @NonNull
     @Override
-    public followReqAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view= LayoutInflater.from(context).inflate(R.layout.follow_request_row,parent,false);
+        final ViewHolder viewHolder=new ViewHolder(view);
+        return new followReqAdapter.ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull followReqAdapter.ViewHolder holder, int position) {
-
+        final Follow follow = followsList.get(position);
+        holder.title.setText(follow.getTitle());
+        holder.state.setText(follow.getState());
+        Glide.with(context.getApplicationContext()).load(follow.getImg()).into(holder.img);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        if (followsList == null) return 0;
+        return followsList.size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
+        TextView title,state;
+        ImageView img;
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+            title=itemView.findViewById(R.id.Request_itemTitle);
+            state=itemView.findViewById(R.id.Request_state);
+            img=itemView.findViewById(R.id.Request_itemImg);
         }
     }
 }
